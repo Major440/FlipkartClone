faa1de1ea877901db7635172875d6f175c0122a4
Tejas <mishratejas46@gmail.com>
2022-06-11 01:51:06 +0530

    v2

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-
 const Connection = async (URL) => {
-   
     try {
         await mongoose.connect(URL, {
+            useUnifiedTopology: true,
             useNewUrlParser: true,
+            useFindAndModify: false,
         });
         console.log("Database connected successfully");
     } catch (err) {
